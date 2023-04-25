86bf0812e39086980baf2a7656d2397bc94f1445
Ritik <raj769417@gmail.com>
2023-04-25 20:59:59 +0530

    implemented error handling

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate=require('ejs-mate');
 const methodOverride = require('method-override');
+
 mongoose.set('strictQuery', false);
 const path=require('path');
 const Campground=require('./models/campground');
 
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 .then(() => console.log("DATABASE CONNECTED."))
@@ -23,38 +26,42 @@ app.get('/home', (req, res) => {
     res.send('<h1>Home</h1>');
 })
 
-app.get("/campgrounds", async (req, res) => {
+app.get("/campgrounds", catchAsync(async (req, res) => {
     const campgrounds=await Campground.find({});
     res.render('campgrounds/home.ejs', { campgrounds });
-})
+}));
 
 app.get("/campgrounds/new", (req, res) => {
     res.render('campgrounds/new.ejs');
 })
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit.ejs', { campground });
-})
+}));
 
-app.post('/campgrounds', async (req, res) => {
-    // console.log(req.body);
+app.post('/campgrounds', catchAsync(async (req, res) => {
+    
+    if(!req.body.campground){
+        console.log('yes');
+        throw new ExpressError('Invalid campground data.', 400);
+    }
     const { campground } = req.body;
     const c = new Campground(campground);
     await c.save();
     res.redirect('/campgrounds');
-})
+}));
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show.ejs', { campground });
-})
+}));
 
-app.patch('/campgrounds/:id/edit', async (req, res) => {
+app.patch('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
+
     const { id } = req.params;
-
     const { campground } = req.body;
 
     const c = await Campground.findByIdAndUpdate(id, {$set: {
@@ -65,18 +72,26 @@ app.patch('/campgrounds/:id/edit', async (req, res) => {
         description: campground.description
     }});
 
-    console.log(c);
-
     res.redirect('/campgrounds');
+}));
 
-    
-})
-
-app.delete("/campgrounds/:id", async (req, res) => {
+app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id);
 
     res.redirect('/campgrounds');
+}));
+
+// its next will call the next middleware which will then send status and message as response
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found !!!', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { status=500 } = err;
+    if(!err.message) err.message = 'Something Went Wrong !!!';
+    res.status(status).render('error', { err });
 })
 
 
---
+++ utils/catchAsync.js
@@ -0,0 +1,6 @@
+const catchAsync = (fn) => {
+    return function(req, res, next) {
+        fn(req, res, next).catch(e => next(e));
+    }
+}
+ module.exports = catchAsync;

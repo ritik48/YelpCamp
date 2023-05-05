f72596e42407de65b47aff1613ff27235c3851d3
Ritik <raj769417@gmail.com>
2023-05-05 23:39:39 +0530

    defined validation schema for review

--- schemas.js
+++ schemas.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-module.exports = Joi.object({
+const campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
@@ -9,3 +9,13 @@ module.exports = Joi.object({
         location: Joi.string().required()
     }).required()
 })
+
+
+const reviewSchema = Joi.object({
+    review: Joi.object({
+        body: Joi.string().required(),
+        rating: Joi.number().required().min(1).max(5)
+    }).required()
+})
+
+module.exports = {campgroundSchema, reviewSchema}

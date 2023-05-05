30aae7e7611c70e8097a857e665f7d779e724e5f
Ritik <raj769417@gmail.com>
2023-05-05 22:19:22 +0530

    added reviews field in the campground model

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const campgroundSchema = new mongoose.Schema({
+const campgroundSchema = new Schema({
     title:String,
     price: Number,
     location: String,
     description: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', campgroundSchema);

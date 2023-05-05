1415ebacc647e470e1d9e6a66f0b9238380d1833
Ritik <raj769417@gmail.com>
2023-05-05 23:37:57 +0530

    defined middleware on campground schema

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Review = require('../models/review');
+
 const campgroundSchema = new Schema({
     title:String,
     price: Number,
@@ -14,6 +16,13 @@ const campgroundSchema = new Schema({
         }
     ]
 });
+
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc) {
+        console.log(doc);
+        await Review.deleteMany({_id: {$in: doc.reviews}});
+    }
+})
 
 module.exports = mongoose.model('Campground', campgroundSchema);
 

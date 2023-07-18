b99e38c6438d32d5e71be37812e39f33e04573b6
Ritik Raj <raj769417@gmail.com>
2023-07-18 17:30:58 +0530

    sanitized user reviews

--- schemas.js
+++ schemas.js
@@ -38,7 +38,7 @@ const campgroundSchema = Joi.object({
 
 const reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5)
     }).required()
 })
